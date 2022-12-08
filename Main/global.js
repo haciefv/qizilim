7b4d344e605c0b6f85d54c4250c13de0f29d16a9
Ali Qasimzade <ali@globalsoft.az>
2022-12-08 12:19:44 +0400

    fixed

--- Main/global.js
+++ Main/global.js
@@ -1,13 +1,40 @@
 const userEmail = localStorage.getItem("Email");
+const twitterWrapper = document.querySelector(".sbx-twitter__wrapper");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const sidebarMenu = document.getElementById("main-menu");
+
+
 if (userEmail) {
   document.getElementById("user").style.display = "block";
   document.getElementById("user").innerHTML = userEmail;
   document.getElementById("user").style.textDecoration = "none";
   document.getElementById("loginRegister").style.display = "none";
+  document.getElementById('info_user').style.display = "flex"
+
 } else {
   document.getElementById("user").style.display = "none";
   document.getElementById("loginRegister").style.display = "block";
+  document.getElementById("info_user").style.display = "none"
+
 }
+
+hamburgerMenu.addEventListener("click", (e) => {
+  console.log(e.target);
+  sidebarMenu.style.visibility = "visible";
+  sidebarMenu.style.left = "0";
+});
+document.getElementById("close-menu-btn").addEventListener("click", () => {
+  sidebarMenu.style.visibility = "hidden";
+  sidebarMenu.style.left = "-250px";
+});
+
+const logoutBtn = document.getElementById('log_out');
+
+logoutBtn.addEventListener('click', () => {
+  localStorage.removeItem('Email')
+  window.location.reload()
+  document.querySelector('.user_info').style.display = "none"
+})
 
 function filterShower() {
   coFilters = document.getElementById("filter-area")
@@ -16,40 +43,55 @@ function filterShower() {
   // coFilters.toggle("filter-block")
   // console.log("AS")
 
-  if (disp=="block"){
-      document.getElementById("filter-area").style.transitionDuration="0.4s"
-      document.getElementById("filter-area").style.display="none"
+  if (disp == "block") {
+    document.getElementById("filter-area").style.transitionDuration = "0.4s"
+    document.getElementById("filter-area").style.display = "none"
   }
-  else{
-      document.getElementById("filter-area").style.display="block"
+  else {
+    document.getElementById("filter-area").style.display = "block"
 
   }
 
 }
+const mediaQuery = window.matchMedia('(max-width: 571px)')
+
+if (mediaQuery.matches) {
+  window.addEventListener('click', (e) => {
+    console.log(e.target.id)
+    if (e.target.id !== "search_button" && e.target.id !== "search_input" && e.target.id !== "reset_button") {
+      document.querySelector('.sbx-custom__input').style.width = "0px"
+      document.querySelector('.sbx-custom__input').style.paddingLeft = "0px"
+    }
+  })
+  twitterWrapper.addEventListener('click', (event) => {
+    console.log(event.target)
+    document.querySelector(".sbx-custom__input").style.width = "120px";
+    document.querySelector('.sbx-custom__input').style.paddingLeft = "8px"
+  })
+}
 
 
+document.querySelector("#main-menu #detailed-search").addEventListener("click", filterShower)
 
-document.querySelector("#main-menu #detailed-search").addEventListener("click",filterShower)
-
-function followReaction(){
+function followReaction() {
   document.getElementById("follow-button").classList.toggle("follow-button-buton-followed")
   document.getElementById("follow-button").classList.toggle("follow-button-buton")
   // followButton.style.background="#FFFFFF"
   // followButton.style.color="#5292e7"
   // followButton.style.border="1px solid #5292e7 "
-  let countFollower=Number(document.getElementById("follower-counter").textContent)
+  let countFollower = Number(document.getElementById("follower-counter").textContent)
   // console.log(countFollower)
   // console.log(countFollower)
-  if (document.getElementById("follow-button").classList[0]=="follow-button-buton-followed"){
-    followButton.innerHTML='Followed'
-    countFollower+=1
+  if (document.getElementById("follow-button").classList[0] == "follow-button-buton-followed") {
+    followButton.innerHTML = 'Followed'
+    countFollower += 1
   }
-  else{
-    countFollower-=1
-    followButton.innerHTML='Follow'
+  else {
+    countFollower -= 1
+    followButton.innerHTML = 'Follow'
   }
-  document.getElementById("follower-counter").innerHTML=countFollower
-  
+  document.getElementById("follower-counter").innerHTML = countFollower
+
 }
 
 
@@ -58,8 +100,8 @@ const loadings = document.querySelectorAll('.loading');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show') 
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
     }
   })
 })
@@ -87,11 +129,11 @@ var swiper2 = new Swiper(".mySwiper2", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  
+
   thumbs: {
     swiper: swiper,
   },
-  
+
 });
 
 
@@ -99,6 +141,6 @@ function EnableDisableTextBox(diamonCheck) {
   var addDiamondCount = document.getElementById("addDiamondCount");
   addDiamondCount.disabled = diamonCheck.checked ? false : true;
   if (!addDiamondCount.disabled) {
-      addDiamondCount.focus();
+    addDiamondCount.focus();
   }
 }
