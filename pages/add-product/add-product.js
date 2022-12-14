01512e3081da400bc861210cfe8234c1b925fc12
Ali Qasimzade <ali@globalsoft.az>
2022-12-15 00:06:04 +0400

    fix

--- pages/add-product/add-product.js
+++ pages/add-product/add-product.js
@@ -4,14 +4,14 @@ const addImgContainer = document.querySelector(".add-img-container");
 let arr = []
 imageInput.addEventListener('change', (e) => {
     arr = Object.values(e.target.files)
-    arr.forEach(it => {
+    arr.forEach((it, index) => {
         const image = document.createElement("img");
         const reader = new FileReader();
         reader.addEventListener("load", () => {
             image.src = reader.result;
             addImgContainer.appendChild(image);
         });
-        reader.readAsDataURL(e.target.files[0]);
+        reader.readAsDataURL(e.target.files[index]);
     })
 
 
